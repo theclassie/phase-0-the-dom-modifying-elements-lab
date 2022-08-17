9a31ed90e0473f7091ad49026bc8b6921d7da1d8
tristyn <trisclassie@gmaiil.com>
2022-08-16 18:37:22 -0600

    commit tree 82fb88d9827e6162f4621762ed2c2bb630b04c2d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.className = 'victory';
+newHeader.id = 'victory';
+// const name = 'Tris';
+
+newHeader.textContent = "YOUR-NAME is the champion"
+
